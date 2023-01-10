b41f1d07c855f381c0723f4fd1c52f8c54adc912
dkchmielewski <66046504+dkchmielewski@users.noreply.github.com>
2023-01-10 07:13:13 -0500

    selecting modal and add button

---
+++ assets/scripts/app.js
@@ -0,0 +1,7 @@
+const addMovieModal = document.getElementById('add-modal');
+
+const startAddMovieButton = document.querySelector('header button');
+
+// modal.addEventListener('click', () => {
+//     if(modal.st)
+// })
